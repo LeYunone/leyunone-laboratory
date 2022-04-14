b669e4bae3e769e04ee74234c03485b1850bcebe
leyunone <365627310@qq.com>
2022-04-14 22:20:31 +0800

    Initial commit

---
+++ src/test/java/xyz/leyuna/leyunalaboratory/LeyunaLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.leyuna.leyunalaboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyunaLaboratoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
