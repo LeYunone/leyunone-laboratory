3543405525ab3c512969859a137756629a817894
leyunone <365627310@qq.com>
2022-05-29 23:43:59 +0800

    [ADD]java -  agentScene agentmain

--- laboratory-core/src/main/java/xyz/leyuna/laboratory/core/agent/AgentScene.java
+++ laboratory-core/src/main/java/xyz/leyuna/laboratory/core/agent/AgentScene.java
@@ -6,7 +6,9 @@ import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 
 import java.io.IOException;
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
 
 /**
  * @author LeYuna
@@ -15,24 +17,37 @@ import java.lang.instrument.Instrumentation;
  */
 public class AgentScene {
 
-    public static void premain(String agentOps, Instrumentation inst){
+    public static void premain(String agentOps, Instrumentation inst) {
         System.out.println("方法一");
         System.out.println(agentOps);
     }
 
-    public static void premain(String agentOps){
+    public static void premain(String agentOps) {
         System.out.println("方法二");
         System.out.println(agentOps);
     }
 
-    public static void agentmain(String agentOps,Instrumentation inst){
+    public static void agentmain(String agentOps, Instrumentation inst) throws UnmodifiableClassException, ClassNotFoundException {
         System.out.println("main方法之后执行");
+        String className = "指定替换的类名";
+        Class[] allLoadedClasses = inst.getAllLoadedClasses();
+        Class tempClazz = null;
+        for (Class clazz : allLoadedClasses) {
+            if (className.equals(clazz.getCanonicalName())) {
+                //需要替换类的类字节码文件
+                tempClazz = clazz;
+            }
+        }
+        ClassDefinition classDefinition = new ClassDefinition(tempClazz,"替换的类字节码的二进制数".getBytes());
+        //替换
+        inst.redefineClasses(classDefinition);
     }
 
     public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
         //如果本虚拟机线程id为1，则监听他
         VirtualMachine attach = VirtualMachine.attach("1");
-        attach.loadAgent("探针jar包的路径","ops参数");
+        //注入agentmain的探针系统
+        attach.loadAgent("探针jar包的路径", "ops参数");
         System.out.println("main");
     }
 }
