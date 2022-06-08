7c0b2cdf1158d33bb38fe5f3a391bc663381dfa9
leyunone <365627310@qq.com>
2022-06-08 23:14:00 +0800

    [ADD]custom RPC

---
+++ laboratory-rpc/src/main/java/xyz/leyuna/laboratory/LaboratoryRpcApplication.java
@@ -0,0 +1,13 @@
+package xyz.leyuna.laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratoryRpcApplication.class, args);
+    }
+
+}
---
+++ laboratory-rpc/src/test/java/xyz/leyuna/laboratory/rpc/LaboratoryRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.leyuna.laboratory.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
