990fd6efd56d6fedbdffbc1d19c824f2814e3263
leyunone <365627310@qq.com>
2023-05-02 16:33:11 +0800

    [update]睡眠排序+

--- laboratory-core/src/main/java/com/leyunone/laboratory/core/algorithm/SleepSort.java
+++ laboratory-core/src/main/java/com/leyunone/laboratory/core/algorithm/SleepSort.java
@@ -4,6 +4,8 @@ package com.leyunone.laboratory.core.algorithm;
  * 沙雕代码 睡眠排序
  */
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * @author LeYunone
  * @email 365627310@qq.com
@@ -17,16 +19,23 @@ public class SleepSort {
 
     public static void sleepSort() {
         int[] arr = new int[]{1, 5, 3, 2, 8,4,100,400,200};
+        CopyOnWriteArrayList<Integer> cwa = new CopyOnWriteArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             long time = arr[i];
+            final int j = i;
             new Thread(() ->{
                 try {
                     Thread.sleep(time);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(time);
+                cwa.add(arr[j]);
             }).start();
+        }
+        while(cwa.size()!=arr.length){
+        }
+        for(Integer num:cwa){
+            System.out.println(num);
         }
     }
 }
