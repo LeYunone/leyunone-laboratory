82e2ea7428edc7fdc0103f8575f8e013da8566e0
leyunone <365627310@qq.copm>
2023-09-20 23:59:35 +0800

    bytebuddy exec

--- laboratory-core/src/main/java/com/leyunone/laboratory/core/bytebuddy/ByteBuddyTest.java
+++ laboratory-core/src/main/java/com/leyunone/laboratory/core/bytebuddy/ByteBuddyTest.java
@@ -1,6 +1,10 @@
 package com.leyunone.laboratory.core.bytebuddy;
 
+import com.leyunone.laboratory.core.bean.Person;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.matcher.ElementMatchers;
 
@@ -12,17 +16,38 @@ import net.bytebuddy.matcher.ElementMatchers;
 public class ByteBuddyTest {
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+        buddyTest1();
+        buddyBuildObject();
+    }
+
+    /**
+     * 方法重写
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    public static void buddyTest1() throws IllegalAccessException, InstantiationException {
         Class<?> dynamicType = new ByteBuddy()
-                .subclass(Object.class)
-                .method(ElementMatchers.named("toString"))
+                .subclass(Hello.class)
+                .method(ElementMatchers.named("testHellow"))
                 .intercept(FixedValue.value("Hello World"))
                 .make()
                 .load(Hello.class.getClassLoader())
                 .getLoaded();
-        Object instance = dynamicType.newInstance();
-        String toString = instance.toString();
-        
+        Hello instance = (Hello)dynamicType.newInstance();
+        String toString = instance.testHellow();
+
         System.out.println(toString);
         System.out.println(instance.getClass().getCanonicalName());
+    }
+
+    public static void buddyBuildObject() throws IllegalAccessException, InstantiationException {
+        Class<? extends Hello> loaded = new ByteBuddy()
+                .subclass(Hello.class)
+                .defineProperty("my",String.class)
+                .defineField("name", String.class,1)
+                .defineField("age", Integer.class,1)
+                .make().load(Hello.class.getClassLoader()).getLoaded();
+        Hello hello = loaded.newInstance();
+        System.out.println(hello.testHellow());
     }
 }
